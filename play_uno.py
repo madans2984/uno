721a75e45ed308d4c0cb877ceb6460ff5df0c4a1
Jules Brettle <jbrettle@olin.edu>
2022-05-09 09:02:24 -0400

    Remove demo deck from play_uno.py

--- play_uno.py
+++ play_uno.py
@@ -7,9 +7,6 @@ from uno_game import GameState, GameDirector
 from uno_controllers import UserPlayerTextController, BotPlayer
 from uno_views import ColorTextView
 
-from testing_helpers import *
-from testing_decks import normal_top_all_actions_early_test_deck
-
 WELCOME_MESSAGE = (
     "\nWelcome to UNO! In this game, each player is dealt seven cards with the gameplay goal being the first to get rid of all of your cards. Each player attempts to match the card in the Discard Pile by the symbol, color, or using a Wild card to change the color of the pile. An example would be if the Discard Pile has a 'Red 0' then players can play either a red card or a 0. If the player was to put down a Wild card they can choose the current color in play. If a player cannot play any cards, they draw one from the pile and it moves to the next person's turn. \n \n"
     "We made two main modifications to our game rules compared to how UNO is typically played. First, we don't have a rule where you need to call UNO once you have your last card. Additionally we decided that special cards cannot be stacked meaning that there would be no cumulative effect for them. \n \n"
@@ -18,12 +15,13 @@ WELCOME_MESSAGE = (
 
 def main():
     # Print the welcome message
-    text = textwrap.TextWrapper(width=80,break_long_words=False,replace_whitespace=False)
+    text = textwrap.TextWrapper(width=80,break_long_words=False,
+        replace_whitespace=False)
     print(text.fill(WELCOME_MESSAGE))
     print()
 
     # Initialize the GameState (contains draw pile, discard pile, and direction)
-    game = GameState(def_deck=pack_deck(normal_top_all_actions_early_test_deck))
+    game = GameState()
 
     # Initialize the players with their hands of 7 cards
     user_player = UserPlayerTextController(game, "User")
